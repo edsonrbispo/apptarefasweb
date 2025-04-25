de04ea17ddc466b0a7712663c36e5c065f029844
Edson Rodrigues <edsonrb.2008@gmail.com>
2025-04-25 16:16:45 -0300

    Preparação do Layout

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,1 +1,29 @@
+@extends('layout')
 
+@section('conteudo')
+    <h2>Lista de Tarefas</h2>
+    <form action="" method="post">
+        <div class="input-group">
+            <input type="text" name="tarefa" class="form-control">
+            <button type="submit" class="btn btn-warning">
+                <i class="fa fa-plus"></i> Adicionar
+            </button>
+        </div>
+    </form>
+
+    <ul class="list-group mt-4">
+        <li class="list-group-item d-flex justify-content-between align-items-center">
+
+            <div class="d-flex">
+                <input type="checkbox" name="status" class="form-check-input me-2">
+                <span>Nome da Tarefa</span>
+            </div>
+            <div class="btn-group">
+                <a href="#" class="btn btn-light btn-sm"><i class="fa fa-edit"></i></a>
+                <form action="#" method="post" class="btn-group">
+                    <button type="submit" class="btn btn-danger btn-sm"><i class="fa fa-trash"></i></button>
+                </form>
+            </div>
+        </li>
+    </ul>
+@endsection
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -6,11 +6,26 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css"
+        integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
     <title>Sistema de Tarefas</title>
 </head>
 
-<body>
-    <h1>Sistema de Tarefas</h1>
+<body class="d-flex flex-column min-vh-100">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark mb-4">
+        <div class="container text-white justify-content-center">
+            <h1>Sistema de Tarefas</h1>
+        </div>
+    </nav>
+    <main class="container flex-grow-1">
+        @yield('conteudo')
+    </main>
+    <footer class="bg-secondary-subtle p-2">
+        <div class="container">
+            <p class="mb-0"><?= date('Y') ?> - Todos os direitos são reservados</p>
+        </div>
+    </footer>
 </body>
 
 </html>
