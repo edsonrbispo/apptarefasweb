7ec89433829629f5b98dffa829345a4686b84bc2
Edson Rodrigues <edsonrb.2008@gmail.com>
2025-05-07 17:16:43 -0300

    Atualização da Index

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -12,18 +12,21 @@
     </form>
 
     <ul class="list-group mt-4">
-        <li class="list-group-item d-flex justify-content-between align-items-center">
 
-            <div class="d-flex">
-                <input type="checkbox" name="status" class="form-check-input me-2">
-                <span>Nome da Tarefa</span>
-            </div>
-            <div class="btn-group">
-                <a href="#" class="btn btn-light btn-sm"><i class="fa fa-edit"></i></a>
-                <form action="#" method="post" class="btn-group">
-                    <button type="submit" class="btn btn-danger btn-sm"><i class="fa fa-trash"></i></button>
-                </form>
-            </div>
-        </li>
+        @foreach ($tarefas as $tr)
+            <li class="list-group-item d-flex justify-content-between align-items-center">
+                <div class="d-flex">
+                    <input type="checkbox" name="status" class="form-check-input me-2" {{ $tr->status ? 'checked' : '' }}>
+                    <span>{{ $tr->tarefa }}</span>
+                </div>
+                <div class="btn-group">
+                    <a href="#" class="btn btn-light btn-sm"><i class="fa fa-edit"></i></a>
+                    <form action="#" method="post" class="btn-group">
+                        <button type="submit" class="btn btn-danger btn-sm"><i class="fa fa-trash"></i></button>
+                    </form>
+                </div>
+            </li>
+        @endforeach
+
     </ul>
 @endsection
